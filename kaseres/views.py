a0ff4a8bfff33af4a60cfaa4d6c14d7394bfd1e5
Mark Wong-VanHaren <markwvh@gmail.com>
2013-04-05 03:25:48 +0200

    adding decorators to view fns

--- kaseres/views.py
+++ kaseres/views.py
@@ -1,19 +1,33 @@
 from django.http import HttpResponse
-
+from django.views.decorators.http import require_http_methods, require_safe, require_POST
 import json
 
-def index(request):
-    # return HttpResponse("Yo yo yo.  KASERES index.")
-    return HttpResponse(json.dumps({'foo': 'bar'}))
+# Check for desired format: HTML or JSON.
+# If JSON, return data.  If HTML, use template.
 
+
+def json_response(obj):
+    return HttpResponse(json.dumps(obj), mimetype="application/json")
+
+
+@require_safe
+def index(request):
+    return json_response({'foo': 'bar'})
+
+# Response: "201 Created" with a Location header
+# containing the URL to the newly created resource.
+@require_POST
 def create_task(request):
     pass
 
+@require_safe
 def read_task(request):
     pass
 
+# @require_PUT
 def update_task(request):
     pass
 
+# @require_DELETE
 def delete_task(request):
     pass
