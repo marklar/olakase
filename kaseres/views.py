326fb62e814c8dd372b54b92c0f5ca9146abef52
Mark Wong-VanHaren <markwvh@gmail.com>
2013-04-07 23:16:23 +0200

    Refactoring views.py to make clearer.

--- kaseres/views.py
+++ kaseres/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.views.decorators.http import require_http_methods, require_safe, require_POST
 from django.template import Context, loader
+from django.shortcuts import render
 from datetime import datetime, date
 
 # from kaseres.models import Task, User, TaskList
@@ -8,35 +9,28 @@ from kaseres.models import Task
 from kaseres.accepts import accepts_json, accepts_html, accepts_xml
 from kaseres.responses import json_models_response, json_obj_response, xml_models_response, xml_obj_response, make_response
 
-# Check for desired format: HTML or JSON.
-# If JSON, return data.  If HTML, use template.
-
 def restify(f):
+    """
+    the view fn must return:
+    { 'models': models,
+      'template': STR,
+      'context': {},
+      'status': 200,   # optional
+      'location: url   # optional
+    }
+    """
     def wrapper(request):
         d = f(request)
-
-        # check whether 'obj' or 'models'
-        # in order to call the right fn
-
         if accepts_json(request):
             return json_models_response(d['models'])
+        # FIXME
         # elif accepts_xml(request):
         elif False:
             return xml_models_response(d['models'])
         else:
-            template = loader.get_template(d['template'])
-            context = Context(d['context'])
-            return HttpResponse(template.render(context))
+            return render(request, d['template'], d['context'])
     return wrapper
 
-# the view fn must return:
-#   { 'models': models,
-#     'template': STR,
-#     'context': {},
-#     'status': 200,   # optional
-#     'location: url   # optional
-#   }
-#
 
 SORT_ATTRS = [
     ('Title', 'title'),
@@ -90,24 +84,63 @@ def create_task(request):
     #     template.render(context), 'text/html', status=201, location=res_url)
     # return json_model_response(task, status=201, location=res_url)
 
-# helper, not a view.
+
+@require_safe
+def index_old(request):
+    hi_pri_tasks = Task.objects.order_by('-priority')[:5]
+    if accepts_json(request):
+        return json_models_response(hi_pri_tasks)
+    elif False: # accepts_xml(request):
+        return xml_models_response(hi_pri_tasks)
+    else:
+        context = {
+            'tasks': hi_pri_tasks,
+            'priorities': Task.PRIORITIES
+        }
+        return render(request, 'tasks/index.html', context)
+        # template = loader.get_template('tasks/index.html')
+        # context = Context({
+        #     'tasks': hi_pri_tasks,
+        #     'priorities': Task.PRIORITIES
+        # })
+        # return HttpResponse(template.render(context))
+
+@require_safe
+def read_task(request, task_id):
+    task = Task.objects.get(task_id)
+    # if accepts_json(request):
+    if True:
+        return json_models_response(task)
+    elif accepts_xml(request):
+        return xml_models_response(task)
+    else:
+        return HttpResponse('<h1>HTML</h1>')
+
+@require_http_methods(['PUT'])
+def update_task(request, task_id):
+    pass
+
+@require_http_methods(['DELETE'])
+def delete_task(request, task_id):
+    Task.objects.get(id=task_id).delete()
+    return HttpResponse('')
+
+# -- helpers --
+
 def get_sort(request):
     sort_prefix = get_sort_prefix(request)
     sort_attr = get_sort_attr(request)
     return sort_prefix + sort_attr
 
-# helper, not a view.
 def get_sort_attr(request):
     a = 'sort_attr'
     return request.GET[a] if a in request.GET else 'priority'
 
-# helper, not a view.
 def get_sort_prefix(request):
     a = 'sort_is_asc'
     is_asc = request.GET[a] == 'true' if a in request.GET else False
     return '' if is_asc else '-'
 
-# helper, not a view.
 def create_task_from_data(request):
     task = Task(title = request.POST['title'],
                 details = request.POST['details'],
@@ -134,56 +167,17 @@ def get_is_completed(request):
         return request.POST['is_completed'] == 'true'
     else:
         return False
-
-@require_safe
-def index_old(request):
-    hi_pri_tasks = Task.objects.order_by('-priority')[:5]
-    if accepts_json(request):
-        return json_models_response(hi_pri_tasks)
-    elif False: # accepts_xml(request):
-        return xml_models_response(hi_pri_tasks)
-    else:
-        template = loader.get_template('tasks/index.html')
-        context = Context({
-            'tasks': hi_pri_tasks,
-            'priorities': Task.PRIORITIES
-        })
-        return HttpResponse(template.render(context))
-
-
-@require_safe
-def read_task(request, task_id):
-    task = Task.objects.get(task_id)
-    # if accepts_json(request):
-    if True:
-        return json_models_response(task)
-    elif accepts_xml(request):
-        return xml_models_response(task)
-    else:
-        return HttpResponse('<h1>HTML</h1>')
-
-@require_http_methods(['PUT'])
-def update_task(request, task_id):
-    pass
-
-
-@require_http_methods(['DELETE'])
-def delete_task(request, task_id):
-    Task.objects.get(id=task_id).delete()
-    return HttpResponse('')
-
-# -- helpers --
-
 def get_index_template(is_ajax):
     if is_ajax:
         return 'tasks/all_tasks.html'
     else:
         return 'tasks/index.html'
 
+# -- unused --
 def all_tasks_html(status=200, location=None):
     # how sorted?
     tasks = Task.objects.all()
-    template = loader.get_template('tasks/all_tasks.html')
+    # template = loader.get_template('tasks/all_tasks.html')
     context = Context({
         'tasks': tasks,
         'priorities': Task.PRIORITIES
